e631345d50fcac4d0e2a14b3bf9ba7b7c09a7476
Raphael Barbo <rsbarbo@gmail.com>
2017-07-01 11:18:28 -0600

    create views with bootstrap and setup, website is up and running with sentry

--- src/crystal_app.cr
+++ src/crystal_app.cr
@@ -1,7 +1,24 @@
 require "kemal"
 
-get "/" do
-  "Hello World!"
+get "/" do |env|
+  title = "Home"
+  page_message = "Welcome to my first Crystal-Lang Website"
+
+  render "views/home.ecr", "views/layouts/standard.ecr"
+end
+
+get "/about" do |env|
+ title="About Me"
+ page_message="Here is a little bit about me..."
+
+ render "views/about.ecr", "views/layouts/standard.ecr"
+end
+
+get "/contact" do |env|
+ title="Contact"
+ page_message="Want to get in touch, here is a little bit of information..."
+
+ render "views/contact.ecr", "views/layouts/standard.ecr"
 end
 
 Kemal.run
